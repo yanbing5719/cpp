f719838fbec7e036b458e0826ad18ae65349a567
yanbing5719 <2814152946@qq.com>
2025-12-26 22:51:49 +0800

    2025.12.26

--- text.cpp
+++ text.cpp
@@ -1,7 +1,11 @@
 #include "iostream"
 using namespace std;
+bool compare(int a,int b){
+return a>b;
+}
 int main()
 {
+  cout<<boolalpha<<compare(2,3)<<endl;    
   cout<<"Hello World"<<endl;
   return 0;  
 } 
