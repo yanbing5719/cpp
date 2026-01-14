c58144948b9f0c791d2ea6a4c5070780200dea9f
yanbing5719 <2814152946@qq.com>
2026-01-14 20:12:00 +0800

    2026.1.14

--- 顺序表.cpp
+++ 顺序表.cpp
@@ -70,6 +70,14 @@ class SqList
     cout<<endl<<endl;
    }
 };
+void print(vector<int>&a)
+{
+    for(int i=0;i<a.size();i++){
+        cout<<a[i]<<' ';
+    }
+    cout<<endl;
+}
+
 #include <iostream>
 #include <vector>
 using namespace std;
