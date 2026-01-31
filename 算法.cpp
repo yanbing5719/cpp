47bc3d7c6830317c475af0f8e15acbaa180a008b
yanbing5719 <2814152946@qq.com>
2026-01-31 12:02:41 +0800

    高精度除法

--- 算法.cpp
+++ 算法.cpp
@@ -140,7 +140,8 @@ for(int i=lc-1;i>=0;i--){
 }
 return 0;
 }*/
-#include <bits/stdc++.h>
+//高精度乘法
+/*#include <bits/stdc++.h>
 using namespace std;
 const int N=1e4;
 int a[N],b[N],c[N];
@@ -171,6 +172,35 @@ int main(){
         b[lb-1-i]=y[i]-'0';
     }
     mul(a,b,c);
+    for(int i=lc-1;i>=0;i--){
+        cout<<c[i];
+    }
+    return 0;
+}*/
+//高精度除法
+#include <bits/stdc++.h>
+using namespace std;
+const int N=1e4;
+int a[N],b,c[N];
+int la,lc;
+void div(int a[],int b,int c[]){
+    long long t=0;
+    for(int i=la-1;i>=0;i--){
+       t=t*10+a[i];
+        c[i]=t/b;
+        t%=b;
+    }
+    while(lc>1&&c[lc-1]==0)lc--;
+}
+int main(){
+    string x;
+    cin>>x>>b;
+    la=x.size();
+    for(int i=0;i<la;i++){
+        a[la-1-i]=x[i]-'0';
+    }
+    lc=la;
+    div(a,b,c);
     for(int i=lc-1;i>=0;i--){
         cout<<c[i];
     }
