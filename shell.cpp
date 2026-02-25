b9421eccc5bf5bcc83f91caea24e5557b7f15026
yanbing5719 <2814152946@qq.com>
2026-02-25 23:35:01 +0800

    实现shell普通命令

--- shell.cpp
+++ shell.cpp
@@ -92,4 +92,84 @@ argv[parse_line.size()]=nullptr;
    }
 }
 
-//
+//主函数
+
+int main()
+{
+    while(1)
+    {
+    string line=read_line();
+    if(line.size()==0)
+    {
+        continue;
+    }
+    vector<string> parse_line=parse(line);
+    int n=parse_line.size();
+    if(parse_line[0]=="exit")
+    {
+     break;
+    }
+
+    //cd
+
+    if(parse_line[0]=="cd")
+    {
+        if(n<2){
+            cout<<"error"<<endl;
+            return 0;
+        }
+        chdir(parse_line[1].c_str());
+        continue;
+    }
+
+    //管道
+    bool ispipe=false;
+    for(int i=0;i<n;i++)
+    {
+        if(parse_line[i]=="|")
+        {
+            ispipe=true;
+        break;
+        }
+    }
+
+    //重定向
+    bool isredirect=false;
+    for(int i=0;i<n;i++)
+    {
+         if (parse_line[i] == ">" ||
+            parse_line[i] == "<" ||
+            parse_line[i] == ">>")
+        {
+            isredirect=true;
+            break;
+    }
+} 
+     if(!ispipe&&!isredirect)
+     {
+        smiple_command(parse_line);
+        continue;
+     }
+     char *argv[64];
+     for(int i=0;i<n;i++)
+{
+  argv[i]=(char*)malloc(parse_line[i].size()+1);
+  strcpy(argv[i],parse_line[i].c_str());
+}
+   argv[parse_line.size()]=nullptr;
+   pid_t pid=fork();
+   if(pid==0)
+   {
+   
+   execvp(argv[0],argv);
+   perror("execvp");
+   _exit(1);
+   }else
+   {
+   waitpid(pid,nullptr,0);
+   }
+   for(int i=0;i<parse_line.size();i++){
+    free(argv[i]);
+   }
+}
+}
